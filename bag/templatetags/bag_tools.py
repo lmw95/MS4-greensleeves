f0978481068526ee0568371b3007eddfff0fe4e3
Lydia <lmwright95@outlook.com>
2022-01-17 22:49:57 +0000

    Calculate and add  estimated delivery to bag

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -1,8 +1,15 @@
 from django import template
-
+from datetime import date, timedelta, datetime
 
 register = template.Library()
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
+
+
+# https://stackoverflow.com/questions/6871016/adding-days-to-a-date-in-python
+@register.filter(name='calc_estimated_delivery')
+def calc_estimated_delivery(days):
+    """Calculate estimated delivery day"""
+    return date.today() + timedelta(days=days)
