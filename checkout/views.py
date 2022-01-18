39da1508069644a9c3b2ba69dc2be8765ea412ce
Lydia <lmwright95@outlook.com>
2022-01-18 16:36:50 +0000

    Minor changes to payment details form

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ import stripe
 
 # Create your views here.
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, 'There is nothing in your bag right now')
@@ -17,12 +20,22 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+                        Did you remember to set it in your environment?')
+
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KJGRPDIv1wyBqdW62WTpYQH32KS1dEjgRJtHpI7oczuqNRD9fd22RPqhLuC5CAmOjxrT0hEUtqYFsJ5eRpbk4fk00Km6zGuWO',
-        'client_secret': 'test',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, 'checkout/checkout.html', context)
