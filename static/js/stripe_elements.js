a056cccee0f574d939300f95369841abacda0bfd
Lydia <lmwright95@outlook.com>
2022-01-26 11:06:34 +0000

    Add functionality that submits checkout form using stripe details

--- static/js/stripe_elements.js
+++ static/js/stripe_elements.js
@@ -1,6 +1,6 @@
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var element = stripe.elements();
 
 var style = {
@@ -22,6 +22,7 @@ var style = {
 var card = element.create('card', {style: style});
 card.mount('#card-element');
 
+
 // Handles realtime stripe card errors
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
@@ -36,4 +37,35 @@ card.addEventListener('change', function (event) {
     } else {
         errorDiv.textContent = "";
     }
+});
+
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
