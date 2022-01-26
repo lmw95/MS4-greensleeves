0ab1d6ea244b89d021efb3b517d67fa729b3550d
Lydia <lmwright95@outlook.com>
2022-01-26 12:43:02 +0000

    Create checkout success page for completed orders

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
+from shop.models import Product
+from .models import Order, OrderLineItem
 from .forms import OrderForm
 from bag.contexts import bag_contents
 
@@ -12,21 +14,63 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, 'There is nothing in your bag right now')
-        return redirect(reverse('shop'))
+    if request.method == "POST":
+        bag = request.session.get('bag', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+            'postcode': request.POST['postcode'],
+            'country': request.POST['country'],
+        }
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in bag.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=item_data,
+                    )
+                    order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        'One of the products in your bag does not exist. '
+                        'Please call us for assistance!'
+                    ))
+                    order.delete()
+                    return redirect(reverse('view_bag'))
+            
+            request.session['save_jnfo'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
 
-    order_form = OrderForm()
+        else:
+            messages.error(request('There is an error with your form. \
+                Please double check your details!'))
+
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, 'There is nothing in your bag right now')
+            return redirect(reverse('shop'))
+
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -39,3 +83,19 @@ def checkout(request):
     }
 
     return render(request, 'checkout/checkout.html', context)
+
+
+def checkout_success(request, order_number):
+    save_info = request.session.get('save-info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order complete! \
+        Your order number is {order_number}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    context = {
+        'order': order,
+    }
+
+    return render(request, 'checkout/checkout-success.html', context)
