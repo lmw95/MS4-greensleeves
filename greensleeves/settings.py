b911fbbd31121945aef197a2b89ad346cb0d8fff
Lydia <lmwright95@outlook.com>
2022-02-07 16:42:11 +0000

    Add profiles urls and views

--- greensleeves/settings.py
+++ greensleeves/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'faqs',
     'about',
     'checkout',
+    'profiles',
 ]
 
 MIDDLEWARE = [
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def profile():
+    """Displays user's profile"""
+    context = {}
+    
+    return render(request, 'profiles/profile.html', context)
