0bf40d4525476bea4260fd34f50dc3895ebe3c18
Lydia <lmwright95@outlook.com>
2022-02-08 00:55:39 +0000

    Enable users to change personal details on profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 def profile(request):
     """Displays user's profile"""
 
     profile = get_object_or_404(UserProfile, user=request.user)
+    phone = profile.default_phone_number
+    line1 = profile.default_street_address1
+    line2 = profile.default_street_address2
+    town = profile.default_town_or_city
+    postcode = profile.default_postcode
+    country = profile.default_country
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     context = {
         'profile': profile,
+        'form': form,
+        'orders': orders,
+        'phone_number': phone,
+        'line1': line1,
+        'line2': line2,
+        'town': town,
+        'postcode': postcode,
+        'country': country,
     }
     
     return render(request, 'profiles/profile.html', context)
--- static/js/script.js
+++ static/js/script.js
@@ -90,4 +90,18 @@ $( document ).ready(function() {
     return new bootstrap.Tooltip(tooltipTriggerEl)
     });
 
+    // Changes countryfield colour
+    let countrySelected = $('#id_default_country').val();
+      if (!countrySelected) {
+          $('#id_default_country').css('color', '#989999');
+      };
+      $('#id_default_country').change(function() {
+        countrySelected = $(this).val();
+        if(!countrySelected) {
+            $(this).css('color', '#989999');
+        } else {
+            $(this).css('color', '#313232');
+        }
+      });
+
 });
