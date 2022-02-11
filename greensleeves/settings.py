9fd79f47b2e1b00676bd18cea99211b386e84f92
Lydia <lmwright95@outlook.com>
2022-02-11 13:51:31 +0000

    Set up postgres database

--- greensleeves/settings.py
+++ greensleeves/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 if os.path.exists('env.py'):
 	import env
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,11 +127,15 @@ WSGI_APPLICATION = 'greensleeves.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+   # 'default': {
+      #  'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse('postgres://ekcsuyfctlzxmj:10332dfae5973c9e7a4b48e0b18ceb0bc944ae2453eed5074cf2a398ecac47a2@ec2-54-220-166-184.eu-west-1.compute.amazonaws.com:5432/dcr7a1qmq59sci')
 }
 
 
