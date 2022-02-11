b9157c7c9fb23fd76cc07970334089a73b229643
Lydia <lmwright95@outlook.com>
2022-02-11 14:14:11 +0000

    Add new database to environment variables

--- greensleeves/settings.py
+++ greensleeves/settings.py
@@ -128,10 +128,7 @@ WSGI_APPLICATION = 'greensleeves.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
