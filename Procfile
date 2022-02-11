724376c92f5182a83c8192a31a128b62426cbd66
Lydia <lmwright95@outlook.com>
2022-02-11 14:32:21 +0000

    Prepare app for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn greensleeves.wsgi:application
--- greensleeves/settings.py
+++ greensleeves/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['lmw95-ms4-greensleeves.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -127,9 +128,18 @@ WSGI_APPLICATION = 'greensleeves.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
