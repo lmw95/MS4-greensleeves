00f8be70a989153b287cc14e850b93c80a2b55b0
Lydia <lmwright95@outlook.com>
2022-02-13 20:41:53 +0000

    Set debug to True for testing purposes

--- greensleeves/settings.py
+++ greensleeves/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT', '')
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['ms4-greensleeves.herokuapp.com', 'localhost']
