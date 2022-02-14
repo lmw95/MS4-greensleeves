ebc58ccd8b55333e370629377a266e58b8b8d5c2
Lydia <lmwright95@outlook.com>
2022-02-14 18:19:10 +0000

    Create Blog/Plant care app

--- profiles/views.py
+++ profiles/views.py
@@ -28,6 +28,7 @@ def profile(request):
 
     else:
         form = UserProfileForm(instance=profile)
+        
     orders = profile.orders.all()
 
     context = {
@@ -46,6 +47,7 @@ def profile(request):
 
 
 def order_history(request, order_number):
+    """Displays user's order history"""
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
