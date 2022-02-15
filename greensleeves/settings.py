80010c37e3f54d3e8d864938ea46b35e8d6d6fd8
Lydia <lmwright95@outlook.com>
2022-02-15 12:58:47 +0000

    Fix mobile nav spacing issue

--- greensleeves/settings.py
+++ greensleeves/settings.py
@@ -16,6 +16,7 @@ if os.path.exists('env.py'):
 import dj_database_url
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
