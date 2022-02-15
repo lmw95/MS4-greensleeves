f35ff46d20dc88d2609d29efaaf693b5bd6c652c
Lydia <lmwright95@outlook.com>
2022-02-15 16:41:48 +0000

    Create blog urls and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def blog(request):
+    """Renders the blog page"""
+    return render(request, 'blog/blog.html')
--- greensleeves/settings.py
+++ greensleeves/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'about',
     'checkout',
     'profiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
