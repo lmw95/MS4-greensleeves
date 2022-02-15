eee5e700a7b16e8316dbd85409a17a82b1ec221f
Lydia <lmwright95@outlook.com>
2022-02-15 18:04:06 +0000

    Add basic blog structure

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
+from blog.views import blog_page
 
 
 urlpatterns = [
     path('', views.blog, name='blog'),
+    path('<slug:slug>', views.blog_page, name='blog_page'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from blog.models import Post
+from .forms import CommentForm
 
 # Create your views here.
 def blog(request):
     """Renders the blog page"""
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'blog/blog.html', context)
+
+
+def blog_page(request, slug):
+    """Renders blog page details"""
+    post = Post.objects.get(slug=slug)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('blog_page', slug=post.slug)
+    
+    else:
+        form = CommentForm
+
+    context = {
+        'post': post,
+        'form': form,
+    }
+
+    return render(request, 'blog/blog-page.html', context)
