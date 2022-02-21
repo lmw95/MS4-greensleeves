36438d87cef7e9f0f673f16b079f0c2d68736604
Lydia <lmwright95@outlook.com>
2022-02-21 16:18:34 +0000

    Finish contact form

--- contact/views.py
+++ contact/views.py
@@ -13,4 +13,38 @@ from .models import Contact
 
 def contact(request):
     """Renders contact form page"""
-    return render(request, 'contact/contact.html')
+
+    contact_request = None
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            contact_request = contact_form.save(commit=False)
+            if request.user.is_authenticated:
+                contact_request.user = request.user
+            contact_request.save()
+            user_email = contact_request.email_address
+            subject = render_to_string(
+                'contact/confirmation_emails/contact_confirmation_email_subject.txt',
+                {'contact_request': contact_request})
+            body = render_to_string(
+                'contact/confirmation_emails/contact_confirmation_email_body.txt',
+                {'contact_request': contact_request, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+            send_mail(
+                subject,
+                body,
+                settings.DEFAULT_FROM_EMAIL,
+                [user_email],
+                fail_silently=False
+            )
+            messages.success(request, 'Your query was submitted successfully.')
+            return redirect(reverse('contact'))
+        else:
+            messages.error(request, 'Your query could not be sent! Please try again.')
+    else:
+        contact_form = ContactForm()
+
+    context = {
+        'contact_form': contact_form,
+    }
+
+    return render(request, 'contact/contact.html', context)
