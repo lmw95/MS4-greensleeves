11aa5b2f86fa3b9e54094424fc16d66e2a665a79
Lydia <lmwright95@outlook.com>
2022-02-23 00:26:33 +0000

    Change development variable

--- greensleeves/settings.py
+++ greensleeves/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEVELOPMENT', '')
 
 
 ALLOWED_HOSTS = ['ms4-greensleeves.herokuapp.com', 'localhost']
@@ -227,8 +227,8 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'greensleeves@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = 'True'
-    EMAIL_POST = 587
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
